78669c49bf44fe924d05457ecd527fd7e8105bc1
unknown <huandrew93@gmail.com>
2019-07-20 21:05:12 -0500

    changed server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express = require('express');
 let serveStatic = require('serve-static');
 app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public/'));
 
 let port = process.env.PORT || 5000;
 app.listen(port);
